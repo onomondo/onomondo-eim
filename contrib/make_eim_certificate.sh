4ddb9db17054b6216cdd08b186bcfaa668eba59d
Philipp Maier <pmaier@sysmocom.de>
2024-07-09 17:48:15 +0200

    sys.config, cosmetic: write NIST in lowercase
    Related: SYS#6720

--- contrib/make_eim_certificate.sh
+++ contrib/make_eim_certificate.sh
@@ -4,8 +4,8 @@
 # openssl. See also: GSMA SGP.32, section 2.6.4.2
 
 #prime256v1: X9.62/SECG curve over a 256 bit prime field
-openssl ecparam -genkey -name prime256v1 -noout -out sample_eim_cert_NIST.key
-openssl req -key sample_eim_cert_NIST.key -new -x509 -days 36500 -out sample_eim_cert_NIST.crt
+openssl ecparam -genkey -name prime256v1 -noout -out sample_eim_cert_nist.key
+openssl req -key sample_eim_cert_nist.key -new -x509 -days 36500 -out sample_eim_cert_nist.crt
 
 #brainpoolP256r1: RFC 5639 curve over a 256 bit prime field
 openssl ecparam -genkey -name brainpoolP256r1 -noout -out sample_eim_cert_brainpool.key
