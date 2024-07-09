4e3853c550c4159b9793ede0c73b1c3f17596948
Philipp Maier <pmaier@sysmocom.de>
2024-07-09 17:28:51 +0200

    contrib:make_eim_certificate.sh
    explain the purpose of this script
    
    Related: SYS#6720

--- contrib/make_eim_certificate.sh
+++ contrib/make_eim_certificate.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-# See also: GSMA SGP.32, section 2.6.4.2
+
+# This script is to illustrate how an eIM certificate could be generated using
+# openssl. See also: GSMA SGP.32, section 2.6.4.2
 
 #prime256v1: X9.62/SECG curve over a 256 bit prime field
 openssl ecparam -genkey -name prime256v1 -noout -out sample_eim_cert_NIST.key
